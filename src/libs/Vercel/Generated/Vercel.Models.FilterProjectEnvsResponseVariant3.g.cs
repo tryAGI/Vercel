8094e8f8bf7b137af87c1da58f01f3de6b34d7e1
Konstantin S. <havendv@gmail.com>
2026-04-19 01:43:44 +0400

    feat: Updated OpenAPI spec (#44)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Vercel/Generated/Vercel.Models.FilterProjectEnvsResponseVariant3.g.cs
+++ src/libs/Vercel/Generated/Vercel.Models.FilterProjectEnvsResponseVariant3.g.cs
@@ -16,6 +16,13 @@ namespace Vercel
         public required global::System.Collections.Generic.IList<global::Vercel.FilterProjectEnvsResponseVariant3Env> Envs { get; set; }
 
         /// <summary>
+        /// 
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("hiddenProductionEnvCount")]
+        [global::System.Text.Json.Serialization.JsonRequired]
+        public required double HiddenProductionEnvCount { get; set; }
+
+        /// <summary>
         /// Additional properties that are not explicitly defined in the schema
         /// </summary>
         [global::System.Text.Json.Serialization.JsonExtensionData]
@@ -25,13 +32,16 @@ namespace Vercel
         /// Initializes a new instance of the <see cref="FilterProjectEnvsResponseVariant3" /> class.
         /// </summary>
         /// <param name="envs"></param>
+        /// <param name="hiddenProductionEnvCount"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public FilterProjectEnvsResponseVariant3(
-            global::System.Collections.Generic.IList<global::Vercel.FilterProjectEnvsResponseVariant3Env> envs)
+            global::System.Collections.Generic.IList<global::Vercel.FilterProjectEnvsResponseVariant3Env> envs,
+            double hiddenProductionEnvCount)
         {
             this.Envs = envs ?? throw new global::System.ArgumentNullException(nameof(envs));
+            this.HiddenProductionEnvCount = hiddenProductionEnvCount;
         }
 
         /// <summary>
