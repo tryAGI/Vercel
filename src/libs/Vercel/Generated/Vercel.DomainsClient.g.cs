9072a0de459033711cb12d260a53eee95b8eb3a8
Konstantin S. <havendv@gmail.com>
2026-08-14 19:58:28 +0400

    feat: Updated OpenAPI spec (#340)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Vercel/Generated/Vercel.DomainsClient.g.cs
+++ src/libs/Vercel/Generated/Vercel.DomainsClient.g.cs
@@ -33,10 +33,17 @@ namespace Vercel
 
         /// <inheritdoc/>
         public global::Vercel.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Vercel.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::Vercel.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
