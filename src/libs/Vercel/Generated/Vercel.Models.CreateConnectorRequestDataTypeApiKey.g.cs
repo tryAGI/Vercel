c4a3c25deba9491ec30c1130e9fe10d123cd82a8
Konstantin S. <havendv@gmail.com>
2026-08-20 05:40:40 +0400

    feat: Updated OpenAPI spec (#358)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Vercel/Generated/Vercel.Models.CreateConnectorRequestDataTypeApiKey.g.cs
+++ src/libs/Vercel/Generated/Vercel.Models.CreateConnectorRequestDataTypeApiKey.g.cs
@@ -22,6 +22,12 @@ namespace Vercel
         public global::System.Collections.Generic.IList<global::Vercel.CreateConnectorRequestDataTypeApiKeyValue>? Values { get; set; }
 
         /// <summary>
+        /// The HTTPS resources the API key authenticates against.
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("serviceUrls")]
+        public global::System.Collections.Generic.IList<string>? ServiceUrls { get; set; }
+
+        /// <summary>
         /// Additional properties that are not explicitly defined in the schema
         /// </summary>
         [global::System.Text.Json.Serialization.JsonExtensionData]
@@ -32,15 +38,20 @@ namespace Vercel
         /// </summary>
         /// <param name="subjectType"></param>
         /// <param name="values"></param>
+        /// <param name="serviceUrls">
+        /// The HTTPS resources the API key authenticates against.
+        /// </param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public CreateConnectorRequestDataTypeApiKey(
             global::Vercel.CreateConnectorRequestDataTypeApiKeySubjectType? subjectType,
-            global::System.Collections.Generic.IList<global::Vercel.CreateConnectorRequestDataTypeApiKeyValue>? values)
+            global::System.Collections.Generic.IList<global::Vercel.CreateConnectorRequestDataTypeApiKeyValue>? values,
+            global::System.Collections.Generic.IList<string>? serviceUrls)
         {
             this.SubjectType = subjectType;
             this.Values = values;
+            this.ServiceUrls = serviceUrls;
         }
 
         /// <summary>
