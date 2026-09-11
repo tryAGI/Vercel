cf485e5e0109e34f51f85bdff2a9e995e150567b
Konstantin S. <havendv@gmail.com>
2026-09-11 15:08:57 +0400

    feat: Updated OpenAPI spec (#416)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Vercel/Generated/Vercel.Models.GetDeploymentResponseVariant2ResourceConfigBuildMachineSelectionSource.g.cs
+++ src/libs/Vercel/Generated/Vercel.Models.GetDeploymentResponseVariant2ResourceConfigBuildMachineSelectionSource.g.cs
@@ -23,6 +23,10 @@ namespace Vercel
         /// <summary>
         ///
         /// </summary>
+        TeamEntitlement,
+        /// <summary>
+        ///
+        /// </summary>
         TeamSetting,
     }
 
@@ -41,6 +45,7 @@ namespace Vercel
                 GetDeploymentResponseVariant2ResourceConfigBuildMachineSelectionSource.ElasticAlgorithm => "elastic-algorithm",
                 GetDeploymentResponseVariant2ResourceConfigBuildMachineSelectionSource.PlanDefault => "plan-default",
                 GetDeploymentResponseVariant2ResourceConfigBuildMachineSelectionSource.ProjectSetting => "project-setting",
+                GetDeploymentResponseVariant2ResourceConfigBuildMachineSelectionSource.TeamEntitlement => "team-entitlement",
                 GetDeploymentResponseVariant2ResourceConfigBuildMachineSelectionSource.TeamSetting => "team-setting",
                 _ => throw new global::System.ArgumentOutOfRangeException(nameof(value), value, null),
             };
@@ -55,6 +60,7 @@ namespace Vercel
                 "elastic-algorithm" => GetDeploymentResponseVariant2ResourceConfigBuildMachineSelectionSource.ElasticAlgorithm,
                 "plan-default" => GetDeploymentResponseVariant2ResourceConfigBuildMachineSelectionSource.PlanDefault,
                 "project-setting" => GetDeploymentResponseVariant2ResourceConfigBuildMachineSelectionSource.ProjectSetting,
+                "team-entitlement" => GetDeploymentResponseVariant2ResourceConfigBuildMachineSelectionSource.TeamEntitlement,
                 "team-setting" => GetDeploymentResponseVariant2ResourceConfigBuildMachineSelectionSource.TeamSetting,
                 _ => null,
             };
